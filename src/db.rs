393e4a530099771625736f940a20789a7690f97b
Guy Taylor <thebigguy.co.uk@gmail.com>
2018-10-05 13:32:00 +0100

    Add more db tests

--- src/db.rs
+++ src/db.rs
@@ -640,12 +640,24 @@ mod test {
 
     #[test]
     fn table_name_string() {
-        assert_eq!(format!("{}", TableName::AlbumsAndMediaItems), "albums_and_media_item");
-        assert_eq!(format!("{:?}", TableName::AlbumsAndMediaItems), "AlbumsAndMediaItems");
+        assert_eq!(
+            format!("{}", TableName::AlbumsAndMediaItems),
+            "albums_and_media_item"
+        );
+        assert_eq!(
+            format!("{:?}", TableName::AlbumsAndMediaItems),
+            "AlbumsAndMediaItems"
+        );
         assert_eq!(format!("{}", TableName::NextInode), "next_inode");
         assert_eq!(format!("{:?}", TableName::NextInode), "NextInode");
-        assert_eq!(format!("{}", TableName::MediaItemsInAlbum), "media_items_in_album");
-        assert_eq!(format!("{:?}", TableName::MediaItemsInAlbum), "MediaItemsInAlbum");
+        assert_eq!(
+            format!("{}", TableName::MediaItemsInAlbum),
+            "media_items_in_album"
+        );
+        assert_eq!(
+            format!("{:?}", TableName::MediaItemsInAlbum),
+            "MediaItemsInAlbum"
+        );
     }
 
     /*
