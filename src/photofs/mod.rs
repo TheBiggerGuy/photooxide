f917921bf3242899518618dc4d18c8b31b2a044f
Guy Taylor <thebigguy.co.uk@gmail.com>
2019-02-16 20:24:55 +0000

    Add tests for photofs::readdir withing albums dir

--- src/photofs/mod.rs
+++ src/photofs/mod.rs
@@ -998,6 +998,42 @@ mod test {
     }
 
     #[test]
+    fn readdir_albums() -> Result<(), FuseError> {
+        let photo_lib = Arc::new(Mutex::new(TestRemotePhotoLib::new()));
+        let photo_db = Arc::new(SqliteDb::in_memory()?);
+        let mut fs = PhotoFs::new(photo_lib.clone(), photo_db.clone());
+
+        let now = Utc::timestamp(&Utc, Utc::now().timestamp(), 0);
+
+        {
+            let fh = fs.opendir(&TestUniqRequest {}, FIXED_INODE_ALBUMS, 0)?.fh;
+
+            let response = fs.readdir(&TestUniqRequest {}, FIXED_INODE_ALBUMS, fh, 0)?;
+            assert_eq!(response.entries.len(), 2);
+            assert_eq!(response.entries[0].ino, FIXED_INODE_ALBUMS);
+            assert_eq!(response.entries[1].ino, FIXED_INODE_ROOT);
+
+            fs.releasedir(&TestUniqRequest {}, FIXED_INODE_ALBUMS, fh, 0)?;
+        }
+
+        let album_inode = photo_db.upsert_album("GoogleId2", "Album1", &now).unwrap();
+
+        {
+            let fh = fs.opendir(&TestUniqRequest {}, FIXED_INODE_ALBUMS, 0)?.fh;
+
+            let response = fs.readdir(&TestUniqRequest {}, FIXED_INODE_ALBUMS, fh, 0)?;
+            assert_eq!(response.entries.len(), 3);
+            assert_eq!(response.entries[0].ino, FIXED_INODE_ALBUMS);
+            assert_eq!(response.entries[1].ino, FIXED_INODE_ROOT);
+            assert_eq!(response.entries[2].ino, album_inode);
+
+            fs.releasedir(&TestUniqRequest {}, FIXED_INODE_ALBUMS, fh, 0)?;
+        }
+
+        Result::Ok(())
+    }
+
+    #[test]
     fn readdir_invalid_inode_or_fh() -> Result<(), FuseError> {
         let photo_lib = Arc::new(Mutex::new(TestRemotePhotoLib::new()));
         let photo_db = Arc::new(SqliteDb::in_memory()?);
